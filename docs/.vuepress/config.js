ce5f2e824e1a3aa7ea099808f007c83cda6688e7
汪杰 <wj344554@alibaba-inc.com>
2023-10-30 15:44:59 +0800

    feat: vuepress配置修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,35 @@
-import { defaultTheme } from '@vuepress/theme-default'
+
+import { defaultTheme } from 'vuepress'
+
 
 export default {
-  base:'/sd-fe-eng/',
+  lang: 'zh-CN',
   title: '前端规范工程化',
+  description: '这是我的第一个 VuePress 站点',
+  base:'/sd-fe-eng/',
   theme: defaultTheme({
     logo: '/img/logo.png',
-    navbar: [
-      // NavbarItem
-      {
-        text: 'Foo',
-        link: '/foo/',
-      },
-      // NavbarGroup
-      {
-        text: 'Group',
-        children: ['/group/foo.md', '/group/bar.md'],
-      },
-      // 字符串 - 页面文件路径
-      '/bar/README.md',
-    ],
-  }),
+  })
 }
+
+// export default {
+//   base:'/sd-fe-eng/',
+//   title: '前端规范工程化',
+//   theme: defaultTheme({
+//     logo: '/img/logo.png',
+//     navbar: [
+//       // NavbarItem
+//       {
+//         text: 'Foo',
+//         link: '/foo/',
+//       },
+//       // NavbarGroup
+//       {
+//         text: 'Group',
+//         children: ['/group/foo.md', '/group/bar.md'],
+//       },
+//       // 字符串 - 页面文件路径
+//       '/bar/README.md',
+//     ],
+//   }),
+// }
