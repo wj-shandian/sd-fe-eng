5e2a348aa5362350cb63a84686f1baca6d2f039b
汪杰 <wj344554@alibaba-inc.com>
2023-12-25 22:25:25 +0800

    feat: 添加eslint-config的测试用例

--- packages/eslint-config/__tests__/eslint-config.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const eslintConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(eslintConfig(), 'Hello from eslintConfig');
-console.info('eslintConfig tests passed');
