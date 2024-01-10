740640daed615d252f9ce18760ba04437c7e55dc
汪杰 <wj344554@alibaba-inc.com>
2024-01-10 10:13:55 +0800

    feat: 删除部分文件

--- packages/sd-fe-lint/__tests__/sd-fe-lint.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const sdFeLint = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(sdFeLint(), 'Hello from sdFeLint');
-console.info('sdFeLint tests passed');
